6a02899b6d96610925ba2bf569f6fcfca3d93968
sisisin <azsisisin@gmail.com>
2015-05-31 05:55:47 -0400

    add menu

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 /// <reference path="typings/node/node.d.ts"/>
+'use strict';
+
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var Menu = require('menu');
+var Twit = require('twit');
+var tokens = require('./tokens.json');
+var T = new Twit(tokens);
 
-// Report crashes to our server.
+
+
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
+
 var mainWindow = null;
 
-// Quit when all windows are closed.
+
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
     app.quit();
 });
 
-// This method will be called when atom-shell has done everything
-// initialization and ready for creating browser windows.
+
 app.on('ready', function() {
+  Menu.setApplicationMenu(menu);
+  
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -26,9 +33,20 @@ app.on('ready', function() {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 });
+
+var template = [
+  {
+    label:'settings',
+    submenu:[
+      { label: 'OAuth', click: function() {  } },
+      { label: 'Toggle DevTools', accelerator: 'F12', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } }
+    ]
+  }
+];
+
+var menu = Menu.buildFromTemplate(template);
+
+
